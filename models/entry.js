61a67615898cf3f401baff7aaccca59476f1ab7f
capomartin <capomartin.a@gmail.com>
2022-01-25 16:33:18 -0300

    add model and migration of entries

---
+++ models/entry.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class entry extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  entry.init({
+    name: DataTypes.STRING,
+    content: DataTypes.STRING,
+    image: DataTypes.STRING,
+    categoryId: DataTypes.INTEGER,
+    type: DataTypes.STRING,
+    deletedAt:DataTypes.DataTypes
+  }, {
+    sequelize,
+    modelName: 'entry',
+  });
+  return entry;
+};

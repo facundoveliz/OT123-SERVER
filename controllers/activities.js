3ad6d28068ac6327ea2f29281eef003505615b99
Iván Rodrigo Ferrioli <ivan.rf.96@gmail.com>
2022-01-25 02:28:32 -0300

    creates getActivities controller

---
+++ controllers/activities.js
@@ -0,0 +1,15 @@
+const models = require("../models");
+const Activities = models.activities;
+
+exports.getActivities = async (req, res, next) => {
+  try {
+    const activities = await Activities.findAll();
+
+    res.status(200).json({
+      message: "Fetched activities successfully.",
+      activities: activities,
+    });
+  } catch (err) {
+    next(err);
+  }
+};

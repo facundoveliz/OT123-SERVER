bea027d85121f20a761ab8655416542fa0dc503f
Iván Rodrigo Ferrioli <ivan.rf.96@gmail.com>
2022-01-25 20:27:21 -0300

    moves activitiesRouter from app.js

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const express = require('express')
 
 const pingRouter = require('./ping')
 const usersRouter = require('./users')
+const activitiesRouter = require('./activities')
 
 const router = new express.Router()
 
 router.use('/ping', pingRouter)
 router.use('/users', usersRouter)
+router.use('/activities', activitiesRouter)
 
 module.exports = router

c34911b4d5d4c5657e3a8b2886704c2991e74b09
martinmolina1988 <67392381+martinmolina1988@users.noreply.github.com>
2022-01-25 06:05:36 -0300

    create model and migration of slider

---
+++ models/Organization.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Organization extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Organization.hasMany(models.Slider, {
+        foreignKey: 'organizationId'
+      })
+    }
+  };
+  Organization.init({
+    name: DataTypes.STRING,
+    image: DataTypes.STRING,
+    phone: DataTypes.STRING,
+    address: DataTypes.STRING,
+    welcome_text: DataTypes.TEXT
+  }, {
+    sequelize,
+    modelName: 'Organization',
+  });
+  return Organization;
+};

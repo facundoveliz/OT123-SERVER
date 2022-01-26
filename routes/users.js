5f1be7017a21425edcfbc2a7552363999c115380
Facundo Veliz <facundoveliz9@gmail.com>
2022-01-25 22:23:20 -0300

    small fixes and code linting

--- routes/users.js
+++ routes/users.js
@@ -1,58 +1,57 @@
-var express = require('express');
+const express = require('express')
+
+const router = express.Router()
+const { body, validationResult } = require('express-validator')
+const bcrypt = require('bcrypt')
+const db = require('../models')
 const user = require('../controllers/user')
-var router = express.Router();
-const db = require("../models");
-const { body, validationResult } = require("express-validator");
-const bcrypt = require("bcrypt");
 
 // get all users.
 router.get('/', user.findAll)
 
 // register a new user.
 router.post(
-  "/register",
-  body("firstName").isLength({ min: 3, max: 24 }),
-  body("lastName").isLength({ min: 3, max: 24 }),
-  body("email").isEmail(),
-  body("password").isLength({ min: 7, max: 128 }),
+  '/register',
+  body('firstName').isLength({ min: 3, max: 24 }),
+  body('lastName').isLength({ min: 3, max: 24 }),
+  body('email').isEmail(),
+  body('password').isLength({ min: 7, max: 128 }),
   async (req, res) => {
     // validation with express-validator
-    const errors = validationResult(req);
+    const errors = validationResult(req)
     if (!errors.isEmpty()) {
       return res.status(400).json({
         ok: false,
-        msg: "Validation error",
+        msg: 'Validation error',
         error: errors.array(),
-      });
+      })
     }
 
     // hash the password
-    const salt = await bcrypt.genSalt(10);
-    const password = await bcrypt.hash(req.body.password, salt);
+    const salt = await bcrypt.genSalt(10)
+    const password = await bcrypt.hash(req.body.password, salt)
 
-    db.User.create({
+    return db.User.create({
       firstName: req.body.firstName,
       lastName: req.body.lastName,
       email: req.body.email,
-      password: password,
+      password,
     })
       .then((newUser) => {
         res.status(201).json({
           ok: true,
-          msg: "User created",
+          msg: 'User created',
           result: newUser,
-        });
+        })
       })
       .catch((err) => {
-        // the error that will catch here will
-        // probably be if the email is repeated
         res.status(400).json({
           ok: false,
-          msg: "The email is already in use",
+          msg: 'The email is already in use',
           error: err,
-        });
-      });
-  }
-);
+        })
+      })
+  },
+)
 
-module.exports = router;
+module.exports = router

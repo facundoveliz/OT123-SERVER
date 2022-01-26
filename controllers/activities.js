27c8fa559be0163848b6615eb244a3470bf9cf9d
Iván Rodrigo Ferrioli <ivan.rf.96@gmail.com>
2022-01-26 00:53:12 -0300

    implements activity creation

--- controllers/activities.js
+++ controllers/activities.js
@@ -1,3 +1,4 @@
+const { validationResult } = require('express-validator')
 const models = require('../models')
 
 const Activities = models.activities
@@ -15,6 +16,35 @@ exports.getActivities = async (req, res) => {
     res.status(400).json({
       ok: false,
       msg: 'error to fetch Activities',
+      error: err,
+    })
+  }
+}
+exports.createActivity = async (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    res.status(422).json({
+      ok: false,
+      msg: 'Validation failed, entered data is incorrect.',
+      error: errors.array(),
+    })
+  }
+  try {
+    const { name, content } = req.body
+
+    const activity = await Activities.create({
+      name,
+      content,
+    })
+    res.status(201).json({
+      ok: true,
+      msg: 'Activity created successfully',
+      result: activity,
+    })
+  } catch (err) {
+    res.status(400).json({
+      ok: false,
+      msg: 'error to create Activity',
       error: err,
     })
   }
--- routes/activities.js
+++ routes/activities.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { body } = require('express-validator')
 
 const router = express.Router()
 
@@ -6,5 +7,18 @@ const activitiesController = require('../controllers/activities')
 
 /* GET activities page. */
 router.get('/', activitiesController.getActivities)
+
+router.post(
+  '/',
+  [
+    body('name')
+      .trim()
+      .isLength({ min: 5 }),
+    body('content')
+      .trim()
+      .isLength({ min: 5 }),
+  ],
+  activitiesController.createActivity,
+)
 
 module.exports = router

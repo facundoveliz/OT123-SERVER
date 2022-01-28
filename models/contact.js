81c32c4f3a9d938c639295c4c22b87bec8776e38
Virginia Gutierrez <virginiagutierrez1986@gmail.com>
2022-01-28 10:35:04 -0300

    add features model contacts

--- models/contact.js
+++ models/contact.js
@@ -1,7 +1,5 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Contact extends Model {
     /**
@@ -12,16 +10,24 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
-  };
-  Contact.init({
-    name: DataTypes.STRING,
-    phone: DataTypes.STRING,
-    email: DataTypes.STRING,
-    message: DataTypes.STRING,
-    deletedAt: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Contact',
-  });
+  }
+  Contact.init(
+    {
+      name: DataTypes.STRING,
+      phone: DataTypes.STRING,
+      email: DataTypes.STRING,
+      message: DataTypes.STRING,
+      deletedAt: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: 'Category',
+      createdAt: 'createAt',
+      updateAt: 'updateAt',
+      deleteAt: 'deleteAt',
+      paranoid: 'true',
+      timestamps: 'true',
+    }
+  );
   return Contact;
 };

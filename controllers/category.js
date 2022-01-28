8219e9dfed1a94d91b6b94d362fc0ed2ec99cab9
Darío Maximiliano Jiménez <dariomaximilianojimenez@gmail.com>
2022-01-28 15:58:01 -0300

    Create categories endpoint & data validation.

--- controllers/category.js
+++ controllers/category.js
@@ -1,3 +1,4 @@
+const { validationResult } = require('express-validator')
 const db = require('../models')
 
 const { Category } = db
@@ -15,6 +16,39 @@ exports.findAll = async (req, res) => {
       ok: false,
       msg: 'You are not authorized to view this information',
       error,
+    })
+  }
+}
+
+exports.add = async (req, res) => {
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    res.status(422).json({
+      ok: false,
+      msg: 'ERROR VALIDATING DATA.',
+      error: errors.array(),
+    })
+  }
+
+  try {
+    const newCategoryData = req.body
+    const newCategory = await Category.create(newCategoryData)
+
+    if (newCategory !== null) {
+      res.status(201).json({
+        ok: true,
+        msg: 'SUCCESS CREATING NEW CATEGORY.',
+        result: { newCategory },
+      })
+    } else {
+      return
+    }
+  } catch (err) {
+    res.status(400).json({
+      ok: false,
+      msg: 'ERROR CREATING NEW CATEGORY.',
+      error: err,
     })
   }
 }

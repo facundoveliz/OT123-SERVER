1a81125686281130ab5cb6ba46e4b4babf200450
Iván Rodrigo Ferrioli <ivan.rf.96@gmail.com>
2022-01-28 02:53:51 -0300

    implements edit categories endpoint

--- routes/categories.js
+++ routes/categories.js
@@ -6,4 +6,6 @@ const router = express.Router()
 /* GET all categories listing. */
 router.get('/', categoriesController.findAll)
 
+router.put('/:id', categoriesController.editCategories)
+
 module.exports = router

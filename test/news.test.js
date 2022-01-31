7d19fe1a06a9298897eeffb2a9b798ea33aefb22
Facundo Veliz <facundoveliz9@gmail.com>
2022-01-30 22:33:55 -0300

    added post news test

--- test/news.test.js
+++ test/news.test.js
@@ -19,7 +19,51 @@ describe('GET /news', () => {
       console.log(res.body.result.news)
       res.body.should.be.a('object')
       .that.includes({ ok: true, msg: 'Fetched news successfully.' })
-      done();
+
+// testing post news
+describe('POST /news/add', () => {
+  // post a new category so the entry can use its id
+  it('respond with a json containing the new categorie created', (done) => {
+    const data = {
+      name: 'Donations',
+      description: 'This is the category where all new donations go',
+    }
+    request(app)
+      .post('/categories/')
+      .send(data)
+      .expect('Content-Type', /json/)
+      .expect(201)
+      .end((err, res) => {
+        if (err) return done(err)
+        res.body.should.be.a('object')
+          .that.includes({ ok: true, msg: 'SUCCESS CREATING NEW CATEGORY.' })
+        done()
+      })
+  })
+
+  // now post the entry using the id of the category
+  it('respond with a json containing the new entry created', (done) => {
+    const data = {
+      name: 'test',
+      content: 'test',
+      image: 'test',
+      categoryId: 1,
+      type: 'test',
+    }
+    request(app)
+      .post('/news/add')
+      .send(data)
+      .expect('Content-Type', /json/)
+      .expect(201)
+      .end((err, res) => {
+        if (err) return done(err)
+        id = res.body.result.entry.dataValues.id
+        res.body.should.be.a('object')
+          .that.includes({ ok: true, msg: 'Entry created' })
+        done()
+      })
+  })
+
     })
   })
 })

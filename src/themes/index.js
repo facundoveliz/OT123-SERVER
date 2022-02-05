16943268bb735858ad12567e30d3d0a429dce649
Facundo Veliz <facundoveliz9@gmail.com>
2022-02-05 18:34:50 -0300

    added colors

--- src/themes/index.js
+++ src/themes/index.js
@@ -4,8 +4,9 @@ import { extendTheme } from '@chakra-ui/react'
 const theme = extendTheme({
   colors: {
     brand: {
-      100: '#f7fafc',
-      900: '#1a202c',
+      red: '#DB5752',
+      yellow: '#FAFA88',
+      lightBlue: '#9AC9FB',
     },
   },
 })

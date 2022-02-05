c2675855bdb6c1b4f6928c4ef02e844fccddcdfd
martinmolina1988 <67392381+martinmolina1988@users.noreply.github.com>
2022-02-05 10:05:10 -0300

    add activitiesService

---
+++ src/service/activitiesService.js
@@ -0,0 +1,59 @@
+import httpService from './http'
+
+const activitiesEndpoint = '/activities'
+
+/**
+ * Accepts an object to send it to activities endpoint
+ * @async
+ * @param {object} activity
+ * @param {string} activity.name
+ * @param {string} activity.image
+ * @param {string} activity.content
+ * @return  Promise {object} of the created activity
+ */
+export function addActivity(activity) {
+  return httpService.post(activitiesEndpoint, activity)
+}
+
+/**
+ * Accepts an id to delete one activity from activities endpoint
+ * @async
+ * @param {int} id
+ * @return Promise {object} of the deleted activity
+ */
+export function delActivity(id) {
+  return httpService.delete(`${activitiesEndpoint}/${id}`)
+}
+
+/**
+ * Accepts an id to retrieve one activity from activities endpoint
+ * @async
+ * @param {int} id
+ * @return Promise {object} of the requested activity
+ */
+export function getActivityById(id) {
+  return httpService.get(`${activitiesEndpoint}/${id}`)
+}
+
+/**
+ * Retrieves an array with all the activities from activities endpoint
+ * @async
+ * @return Promise [{object}] of all the requested activities
+ */
+export function getAllActivities() {
+  return httpService.get(activitiesEndpoint)
+}
+
+/**
+ * Accepts an object to send it to activities endpoint
+ * @async
+ * @param {int} id {int}
+ * @param {object} activity
+ * @param {string} activity.name
+ * @param {string} activity.image
+ * @param {string} activity.content
+ * @return Promise {object} of the created activity
+ */
+export function updateActivity(id, activity) {
+  return httpService.put(`${activitiesEndpoint}/${id}`, activity)
+}

c19b94f1a73c93c16695fa8f8eec012f9e20495a
Facundo Veliz <facundoveliz9@gmail.com>
2022-02-08 19:39:14 -0300

    added user list table

---
+++ src/pages/backoffice/users/ListUsers.jsx
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from 'react'
+import { IoTrashBin, IoPencil } from 'react-icons/io5'
+import {
+  Box,
+  Table,
+  Heading,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  Button,
+  ButtonGroup,
+} from '@chakra-ui/react'
+import { getAllUsers } from '../../../services/usersService'
+
+const ListUsers = () => {
+  const [usersData, setUsersData] = useState([]);
+  /* with this warning eslint wants you to make
+  *  the async function inside the useEffect, but
+  *  we don't want that because we have an entire
+  *  folder for that
+  */
+  /* eslint-disable react-hooks/exhaustive-deps */
+  useEffect(async () => {
+    const res = await getAllUsers()
+    setUsersData(res.data.result.user)
+  }, []);
+
+  return (
+    <Box display="flex" height="100vh" width="100%" backgroundColor="#FAFA88">
+      <Box
+        borderWidth="1px solid white"
+        borderRadius="lg"
+        boxShadow="lg"
+        backgroundColor="white"
+        w="90%"
+        h="max-content"
+        m="auto"
+        p="2"
+        justifyContent="center"
+        overflow="hidden"
+      >
+        <Heading align="center">Users</Heading>
+        <Table>
+          <Thead>
+            <Tr>
+              <Th>Nombre</Th>
+              <Th>Apellido</Th>
+              <Th>Email</Th>
+              <Th>Imagen</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {usersData.map((item) => (
+              <Tr key={item.id}>
+                <Td>{item.firstName}</Td>
+                <Td>{item.lastName}</Td>
+                <Td>{item.email}</Td>
+                <Td>{item.image}</Td>
+                <Td maxWidth="120px">
+                  <ButtonGroup
+                    display="flex"
+                    flexWrap="wrap"
+                    textAlign="center"
+                    spacing="0"
+                    width="fit-content"
+                  >
+                    <Button
+                      width="100px"
+                      leftIcon={<IoPencil />}
+                      marginRight="6"
+                      marginBottom="1"
+                      size="sm"
+                    >
+                      Editar
+                    </Button>
+                    <Button width="100px" leftIcon={<IoTrashBin />} size="sm">
+                      Eliminar
+                    </Button>
+                  </ButtonGroup>
+                </Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </Box>
+    </Box>
+  )
+}
+
+export default ListUsers

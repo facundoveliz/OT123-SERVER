abc97a78a3ab6715d3ab9a284300025850f5427f
Darío Maximiliano Jiménez <dariomaximilianojimenez@gmail.com>
2022-02-10 09:11:52 -0300

    SweetAlert2 & design changes.

--- src/router/index.js
+++ src/router/index.js
@@ -10,13 +10,17 @@ import App from '../App'
 // import Home from '../pages/Home'
 import SignIn from '../pages/signin/SignInForm'
 import SignUp from '../pages/signup/SignUpForm'
+import Home from '../pages/Home'
+import TestimonialForm from '../components/testimonials/TestimonialForm'
 
 const AllRoutes = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} />
+      <Route path="/" element={<Home />} />
       <Route path="/signin" element={<SignIn />} />
       <Route path="/signup" element={<SignUp />} />
+      <Route path="/testimonialform" element={<TestimonialForm />} />
+      <Route path="/app" element={<App />} />
     </Routes>
   </BrowserRouter>
 )

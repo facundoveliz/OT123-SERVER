2657b2754accc0ef23074e926c535467490a3ec4
martinmolina1988 <67392381+martinmolina1988@users.noreply.github.com>
2022-02-16 23:53:54 -0300

    changes in imgUploadServices

--- src/services/imgUploadService.js
+++ src/services/imgUploadService.js
@@ -1,15 +1,14 @@
+/* eslint-disable no-console */
 /* eslint-disable quotes */
 /* eslint-disable global-require */
-/* eslint-disable no-console */
 import S3 from 'react-aws-s3'
 
-const fileName = () => {
+const fileName = (file) => {
   window.Buffer = window.Buffer || require("buffer").Buffer;
   const uuid = crypto.randomUUID() // Generate a Universally Unique Identifier.
-  // const fileNameArr = file.name.split('.') // Split the file name into arrays when it finds '.'
-  // const fileExtensions = fileNameArr[fileNameArr.length - 1] // Extract just the file extension.
+  const fileNameArr = file.name.split('.') // Split the file name into arrays when it finds '.'
+  const fileExtension = fileNameArr[fileNameArr.length - 1] // Extract just the file extension.
 
-  const fileExtension = 'jpg'
   const uniqueFileName = `${uuid}.${fileExtension}` // Create the new Unique filename.
   return uniqueFileName
 }
@@ -25,11 +24,12 @@ const config = {
 const ReactS3Client = new S3(config);
 
 export default async function UploadFile(file) {
-  const newFileName = fileName()
+  const newFileName = fileName(file)
+  console.log(newFileName);
   try {
     if (file) {
       const respuesta = await ReactS3Client.uploadFile(file, newFileName)
-      console.log(respuesta);
+      return respuesta.location
     }
   } catch (error) {
     console.log(error);

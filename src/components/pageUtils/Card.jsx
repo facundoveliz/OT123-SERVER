2935079bdb9ba79c08a6a497b2a31e55a61ac646
martinmolina1988 <67392381+martinmolina1988@users.noreply.github.com>
2022-02-18 14:55:39 -0300

    create reusable components Cards and Details

---
+++ src/components/pageUtils/Card.jsx
@@ -0,0 +1,76 @@
+/* eslint-disable react/prop-types */
+import React from 'react'
+import { GridItem, Text, Image } from '@chakra-ui/react'
+import { useNavigate } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+const Card = ({ array, direction }) => {
+  const navigate = useNavigate()
+
+  return (
+    <>
+      {array.map((item) => (
+        <GridItem
+          key={item.id}
+          w="350px"
+          borderRadius="lg"
+          boxShadow="lg"
+          borderWidth="1px"
+          cursor="pointer"
+          textAlign="justify"
+          onClick={() => {
+            navigate(`/${direction}/${item.id}`)
+          }}
+        >
+          <Text
+            fontSize="2xl"
+            px={6}
+            pt={3}
+            pb={3}
+            whiteSpace="nowrap"
+            overflow="hidden"
+            textOverflow="ellipsis"
+          >
+            {item.name}
+
+          </Text>
+          <Text
+            px={6}
+            pt={3}
+            pb={3}
+            whiteSpace="nowrap"
+            overflow="hidden"
+            textOverflow="ellipsis"
+          >
+            {item.content}
+
+          </Text>
+          <Text
+            px={6}
+            pt={3}
+            pb={3}
+            whiteSpace="nowrap"
+            overflow="hidden"
+            textOverflow="ellipsis"
+          >
+            {item.description}
+
+          </Text>
+          <Image src={item.image} />
+        </GridItem>
+      ))}
+    </>
+  )
+}
+Card.propTypes = {
+  direction: PropTypes.string.isRequired,
+  array: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      image: PropTypes.string,
+      content: PropTypes.string,
+    }),
+  ).isRequired,
+
+}
+export default Card

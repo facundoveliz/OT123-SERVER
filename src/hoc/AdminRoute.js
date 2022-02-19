56631e510abb2b879fa018622bd7fb42efc9f8d4
martinmolina1988 <67392381+martinmolina1988@users.noreply.github.com>
2022-02-19 18:05:50 -0300

    fix admin route

--- src/hoc/AdminRoute.js
+++ src/hoc/AdminRoute.js
@@ -5,6 +5,7 @@ import { Outlet } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { getUserData } from '../app/slices/auth'
 import useUser from '../hooks/useUser'
+import NotFound from '../pages/notFound/NotFound'
 
 const AdminRoute = () => {
   const userData = useSelector(getUserData)
@@ -13,7 +14,7 @@ const AdminRoute = () => {
   if (!isLoggedIn) {
     role = userData.payload.persistedReducer.userData?.dataValues.roleId
   }
-  
+
   return (
 
     role === 1 ? <Outlet /> : <NotFound />

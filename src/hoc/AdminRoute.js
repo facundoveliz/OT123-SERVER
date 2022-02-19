9e14efa1e5d7e14eff72c0301c21d4f97f02e2cc
martinmolina1988 <67392381+martinmolina1988@users.noreply.github.com>
2022-02-19 17:56:59 -0300

    fix roleId

--- src/hoc/AdminRoute.js
+++ src/hoc/AdminRoute.js
@@ -1,14 +1,20 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
 import React from 'react'
-import { Navigate, Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { getUserData } from '../app/slices/auth'
+import useUser from '../hooks/useUser'
 
 const AdminRoute = () => {
   const userData = useSelector(getUserData)
-  const { roleId } = userData.payload.userData.dataValues
-
-  if (roleId !== 1) {
-    return <Navigate replace to="/" />
+  const { isLoggedIn } = useUser()
+  let role = 0
+  if (!isLoggedIn) {
+    role = userData.payload.persistedReducer.userData?.dataValues.roleId
+  }
+  if (role !== 1) {
+    return <h2>No tienes permiso</h2>
   }
   return <Outlet />
 }

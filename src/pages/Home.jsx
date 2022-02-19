7bc49f00591d406dbdde8f838337f7363abd9534
martinmolina1988 <67392381+martinmolina1988@users.noreply.github.com>
2022-02-19 17:23:43 -0300

    change in Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,17 +3,13 @@ import {
   Img,
   Text,
 } from '@chakra-ui/react'
-import Header from '../components/navbar/Header'
-import Footer from '../components/Footer'
 
 const Home = () => (
   <>
-    <Header />
     <Img src="/ong-pics/Foto1.jpg" w="100%" h="60vh" objectFit="cover" />
     <Text textAlign="center" fontSize="3xl" my={5}>Texto de Bienvenida</Text>
     <Text textAlign="center" fontSize="2xl" mb={2}>Últimas novedades</Text>
     <Text textAlign="center" fontSize="2xl">Testimonios</Text>
-    <Footer />
   </>
 )
 

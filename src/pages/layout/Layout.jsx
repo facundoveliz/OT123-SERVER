e4b0b365009b24082801df1dd62f5b5328908855
martinmolina1988 <67392381+martinmolina1988@users.noreply.github.com>
2022-02-19 17:24:26 -0300

    add alert in layout

--- src/pages/layout/Layout.jsx
+++ src/pages/layout/Layout.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import { useSelector } from 'react-redux';
+import Alert from '../../components/alert/Alert'
 import Footer from '../../components/Footer'
 import Header from '../../components/navbar/Header'
+import { getAlertData } from '../../app/slices/alert';
 
-const Layout = () => (
-  <div>
-    <Header />
-    <Outlet />
-    <Footer />
-  </div>
-)
+const Layout = () => {
+  const alertData = useSelector(getAlertData)
+
+  const alert = alertData.payload.alert.alertData
+
+  return (
+    <div>
+      <Alert {...alert} />
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
 
 export default Layout

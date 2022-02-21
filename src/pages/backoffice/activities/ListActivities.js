88068dae6ef2a49ff1549049356ff61793516a80
martinmolina1988 <67392381+martinmolina1988@users.noreply.github.com>
2022-02-21 20:33:34 -0300

    add endpoint listactivities

--- src/pages/backoffice/activities/ListActivities.js
+++ src/pages/backoffice/activities/ListActivities.js
@@ -1,5 +1,4 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { useEffect, useState } from 'react'
 import { IoTrashBin, IoPencil } from 'react-icons/io5'
 import {
   Box,
@@ -13,68 +12,69 @@ import {
   Button,
   ButtonGroup,
 } from '@chakra-ui/react'
+import loadListData from '../allListData'
 
-const ListActivities = ({ activitiesData }) => (
-  <Box display="flex" height="100vh" width="100%" backgroundColor="#FAFA88">
-    <Box
-      borderWidth="1px solid white"
-      borderRadius="lg"
-      boxShadow="lg"
-      backgroundColor="white"
-      w={{ base: '90%', md: 600 }}
-      h="max-content"
-      m="auto"
-      p="2"
-      justifyContent="center"
-      overflow="hidden"
-    >
-      <Heading align="center">Activities</Heading>
-      <Table>
-        <Thead>
-          <Tr>
-            <Th>Nombre</Th>
-            <Th>Acciones</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {activitiesData.map((item) => (
-            <Tr key={item.id}>
-              <Td>{item.name}</Td>
-              <Td maxWidth="120px">
-                <ButtonGroup
-                  display="flex"
-                  flexWrap="wrap"
-                  textAlign="center"
-                  spacing="0"
-                  width="fit-content"
-                >
-                  <Button
-                    width="100px"
-                    leftIcon={<IoPencil />}
-                    marginRight="6"
-                    marginBottom="1"
-                    size="sm"
-                  >
-                    Editar
-                  </Button>
-                  <Button width="100px" leftIcon={<IoTrashBin />} size="sm">
-                    Eliminar
-                  </Button>
-                </ButtonGroup>
-              </Td>
+const ListActivities = () => {
+  const [activitiesData, setActivitiesData] = useState([])
+  useEffect(() => {
+    loadListData('actividades').then(({ activities }) => setActivitiesData(activities))
+  }, [])
+
+  return (
+    <Box display="flex" height="100vh" width="100%" backgroundColor="#FAFA88">
+      <Box
+        borderWidth="1px solid white"
+        borderRadius="lg"
+        boxShadow="lg"
+        backgroundColor="white"
+        w={{ base: '90%', md: 600 }}
+        h="max-content"
+        m="auto"
+        p="2"
+        justifyContent="center"
+        overflow="hidden"
+      >
+        <Heading align="center">Activities</Heading>
+        <Table>
+          <Thead>
+            <Tr>
+              <Th>Nombre</Th>
+              <Th>Acciones</Th>
             </Tr>
-          ))}
-        </Tbody>
-      </Table>
+          </Thead>
+          <Tbody>
+            {activitiesData.map((item) => (
+              <Tr key={item.id}>
+                <Td>{item.name}</Td>
+                <Td maxWidth="120px">
+                  <ButtonGroup
+                    display="flex"
+                    flexWrap="wrap"
+                    textAlign="center"
+                    spacing="0"
+                    width="fit-content"
+                  >
+                    <Button
+                      width="100px"
+                      leftIcon={<IoPencil />}
+                      marginRight="6"
+                      marginBottom="1"
+                      size="sm"
+                    >
+                      Editar
+                    </Button>
+                    <Button width="100px" leftIcon={<IoTrashBin />} size="sm">
+                      Eliminar
+                    </Button>
+                  </ButtonGroup>
+                </Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </Box>
     </Box>
-  </Box>
-)
-ListActivities.propTypes = {
-  activitiesData: PropTypes.arrayOf(
-    PropTypes.shape({
-      name: PropTypes.string,
-    }),
-  ).isRequired,
+  )
 }
 
 export default ListActivities

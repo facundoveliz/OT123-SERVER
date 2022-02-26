3aebd1ba3dd6c4b81cfb05ca0bbbcab684fc06bf
martinmolina1988 <67392381+martinmolina1988@users.noreply.github.com>
2022-02-26 19:25:22 -0300

    create title component

---
+++ src/components/pageUtils/Title/Title.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './Title.css'
+import { Text } from '@chakra-ui/layout'
+
+const Title = ({ title, fontSize }) => (
+  <div className="paint-stroke">
+    <Text fontSize={fontSize} className="paint-text">{title}</Text>
+
+  </div>
+)
+
+Title.propTypes = {
+  title: PropTypes.string.isRequired,
+  fontSize: PropTypes.number,
+}
+Title.defaultProps = {
+  fontSize: 25,
+}
+export default Title

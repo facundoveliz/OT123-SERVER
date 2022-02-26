3fb534534f36fcee0aeb38010e0b03897ad51a3e
martinmolina1988 <67392381+martinmolina1988@users.noreply.github.com>
2022-02-25 22:55:22 -0300

    change listCategories

--- src/pages/backoffice/categories/ListCategories.js
+++ src/pages/backoffice/categories/ListCategories.js
@@ -1,24 +1,16 @@
 import React, { useEffect, useState } from 'react'
-import { IoTrashBin, IoPencil, IoAddOutline } from 'react-icons/io5'
+import { IoAddOutline } from 'react-icons/io5'
 import {
-  Box,
-  Text,
-  Table,
-  Heading,
-  Tbody,
-  UnorderedList,
-  ListItem,
-  Button,
-  ButtonGroup,
-  Collapse,
-  useDisclosure,
+  Box, Table, Heading, Tbody, Button,
 } from '@chakra-ui/react'
 import { useNavigate } from 'react-router'
-import { deleteCategory, getAllCategories } from '../../../services/categoriesService'
+import {
+  getAllCategories,
+} from '../../../services/categoriesService'
 import Alert from '../../../components/alert/Alert'
+import ItemCollapse from './ItemCollapse'
 
 const ListCategories = () => {
-  const { isOpen, onToggle } = useDisclosure()
   const [allCategories, setAllCategories] = useState([{}])
   const [deletedCategory, setDeletedCategory] = useState([])
   const [alertProps, setAlertProps] = useState({
@@ -46,53 +38,18 @@ const ListCategories = () => {
     }
   }
 
-  const confirmDelete = async (id) => {
-    try {
-      const confirmedDelete = await deleteCategory(id)
-      if (confirmedDelete) {
-        setAlertProps({
-          show: true,
-          title: 'Categoria Eliminada!',
-          message: 'Categoria eliminada de forma exitosa!',
-          icon: 'success',
-          cancelbtn: false,
-          onConfirm: () => {
-            setDeletedCategory(id)
-            window.location.reload();
-          },
-        })
-      }
-    } catch (error) {
-      setAlertProps({
-        show: true,
-        title: 'Hubo un error!',
-        message: error.message,
-        icon: 'error',
-        cancelbtn: true,
-        onConfirm: () => {},
-        onCancel: () => {},
-      })
-    }
-  }
-
-  const handleDelete = (id) => {
-    setAlertProps({
-      show: true,
-      title: 'Estas Seguro?',
-      message: 'Esta acción es permanente. ¿Eliminar categoria?',
-      icon: 'warning',
-      cancelbtn: true,
-      onConfirm: () => confirmDelete(id),
-      onCancel: () => {},
-    })
-  }
-
   useEffect(() => {
     loadData()
   }, [deletedCategory])
 
   return (
-    <Box display="flex" height="100%" width="100%" backgroundColor="#f2f2f2" justifyContent="center">
+    <Box
+      display="flex"
+      height="100%"
+      width="100%"
+      backgroundColor="#f2f2f2"
+      justifyContent="center"
+    >
       <Alert {...alertProps} />
       <Box
         border="2px solid black"
@@ -122,92 +79,11 @@ const ListCategories = () => {
         <Table size="lg">
           <Tbody>
             {allCategories.map((item) => (
-              <>
-                <Box
-                  textAlign="center"
-                  padding="1"
-                >
-                  <Button
-                    w="100%"
-                    border="2px solid black"
-                    onClick={onToggle}
-                  >
-                    {item.name}
-                  </Button>
-                </Box>
-                <Collapse startingHeight={5} in={isOpen} animateOpacity>
-                  <Box
-                    border="2px solid black"
-                    marginBottom="8"
-                    p="20px"
-                    color="white"
-                    mt="4"
-                    bg="#d6f5d6"
-                    rounded="md"
-                    shadow="md"
-                  >
-                    <UnorderedList key={item.id} color="black">
-                      <ListItem>
-                        <Text fontWeight="bold">
-                          Descripción:
-                        </Text>
-                        {' '}
-                        {item.description}
-                      </ListItem>
-                      <ListItem>
-                        <Text fontWeight="bold">
-                          Creado:
-                        </Text>
-                        {' '}
-                        {item.createdAt}
-                      </ListItem>
-                      <ListItem>
-                        <Text fontWeight="bold">
-                          Actualizado:
-                        </Text>
-                        {' '}
-                        {item.updatedAt}
-                      </ListItem>
-                      <ListItem display="flex" justifyContent="center">
-                        <ButtonGroup
-                          paddingTop="4"
-                          flexWrap="wrap"
-                          textAlign="center"
-                          width="fit-content"
-                        >
-                          <Button
-                            border="2px solid black"
-                            width="100px"
-                            leftIcon={<IoPencil />}
-                            marginBottom="1"
-                            size="sm"
-                            backgroundColor="#ccebff"
-                            _hover={{
-                              backgroundColor: '#4db8ff',
-                            }}
-                            onClick={() => navigate(`./${item.id}`)}
-                          >
-                            Editar
-                          </Button>
-                          <Button
-                            border="2px solid black"
-                            width="100px"
-                            leftIcon={<IoTrashBin />}
-                            size="sm"
-                            backgroundColor="#ffc2b3"
-                            _hover={{
-                              backgroundColor: '#ff4d4d',
-                            }}
-                            onClick={() => handleDelete(item.id)}
-                          >
-                            Eliminar
-                          </Button>
-                        </ButtonGroup>
-                      </ListItem>
-                    </UnorderedList>
-                  </Box>
-                </Collapse>
-              </>
+              <ItemCollapse
+                item={item}
+                setAlertProps={setAlertProps}
+                setDeletedCategory={setDeletedCategory}
+              />
             ))}
           </Tbody>
         </Table>

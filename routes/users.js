696058700c8423a1ae041efe049a9d975f116d1f
Darío Maximiliano Jiménez <dariomaximilianojimenez@gmail.com>
2022-03-01 12:18:24 -0300

    BUG FIXED.

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
 const {
-  getAll, userData, signup, signin, editUser, deleteUser,
+  getAll, userData, signup, signin, getUserById, editUser, deleteUser,
 } = require('../controllers/users')
 const verifyToken = require('../middlewares/verifyToken')
 const validate = require('../schemas/userSchema')
@@ -12,6 +12,8 @@ const router = express.Router()
 router.get('/', getAll)
 
 router.get('/auth/me', verifyToken, userData)
+
+router.get('/:id', getUserById)
 
 // register a new
 router.post('/signup', validate, signup)

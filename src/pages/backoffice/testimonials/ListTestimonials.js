bb2e1412a1da189efd9b1c674287942fb1d7a476
Facundo Veliz <facundoveliz9@gmail.com>
2022-03-02 15:53:18 -0300

    added list testimonials infinite scroll

--- src/pages/backoffice/testimonials/ListTestimonials.js
+++ src/pages/backoffice/testimonials/ListTestimonials.js
@@ -11,7 +11,7 @@ import moment from 'moment';
 import { setAlertData } from '../../../app/slices/alert';
 import Title from '../../../components/pageUtils/Title/Title';
 import useUser from '../../../hooks/useUser';
-import { addTestimonial, getAllTestimonials } from '../../../services/testimonialsService';
+import { addTestimonial, getTestimonialPagination } from '../../../services/testimonialsService';
 
 const backgrounds = [
   'url("data:image/svg+xml, %3Csvg xmlns=\'http://www.w3.org/2000/svg\' width=\'560\' height=\'185\' viewBox=\'0 0 560 185\' fill=\'none\'%3E%3Cellipse cx=\'102.633\' cy=\'61.0737\' rx=\'102.633\' ry=\'61.0737\' fill=\'%23ED64A6\' /%3E%3Cellipse cx=\'399.573\' cy=\'123.926\' rx=\'102.633\' ry=\'61.0737\' fill=\'%23F56565\' /%3E%3Cellipse cx=\'366.192\' cy=\'73.2292\' rx=\'193.808\' ry=\'73.2292\' fill=\'%2338B2AC\' /%3E%3Cellipse cx=\'222.705\' cy=\'110.585\' rx=\'193.808\' ry=\'73.2292\' fill=\'%23ED8936\' /%3E%3C/svg%3E")',
@@ -110,12 +110,30 @@ export default function ListTestimonials() {
   const { isLoggedIn } = useUser()
   const userId = useUser().userData.payload.persistedReducer.userData?.dataValues.id
   const dispatch = useDispatch()
+
+  let currentPage = 0
   const loadData = () => {
-    getAllTestimonials().then((result) => setAllTestimonials(result.data.result.testimonials))
+    getTestimonialPagination(20, currentPage)
+      .then((result) => setAllTestimonials(
+        (prev) => [...prev, ...result.data.result.rows],
+      ))
+    currentPage += 1
   }
+
+  const handleScroll = (e) => {
+    const { scrollHeight } = e.target.documentElement;
+    const currentHeight = Math.ceil(
+      e.target.documentElement.scrollTop + window.innerHeight,
+    );
+    if (currentHeight + 1 >= scrollHeight) {
+      loadData();
+    }
+  };
 
   useEffect(() => {
     loadData()
+    window.addEventListener('scroll', handleScroll)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [onLoading])
 
   const onChange = (e) => {
